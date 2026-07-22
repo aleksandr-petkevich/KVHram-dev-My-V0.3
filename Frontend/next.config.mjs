e07fecec59fcc3ac107087a4220439ff99e43574
Aleksandr Petkevich <alex.petkevich@mail.ru>
2026-07-22 13:05:42 +0300

    rewrites for frontend

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,3 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//   // Add the EXACT hostname from your tunnel here
+//   allowedDevOrigins: ['4c50f690cf2c30.lhr.life'],
+// }
+
+// export default nextConfig
+
+// для vercel
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -8,6 +23,16 @@ const nextConfig = {
   },
   // Add the EXACT hostname from your tunnel here
   allowedDevOrigins: ['4c50f690cf2c30.lhr.life'],
-}
 
-export default nextConfig
+  // 🔽 НОВЫЙ БЛОК: Прокси для API
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://backend-kv-hram-dev-my-v0-3-y2sj-lfdt0pz3q.vercel.app/api/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
